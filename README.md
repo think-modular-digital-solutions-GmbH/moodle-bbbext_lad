70236ab6ecd56cfc9d39db502c777c65e32a067d
Stefan Weber <stefan.weber@think-modular.com>
2026-01-26 17:19:38 +0100

    Working v1.0

---
+++ README.md
@@ -0,0 +1,7 @@
+## Installation
+- install the local_bbb_lad plugin
+- put this plugin into /mod/bigbluebuttonbn/extensions
+
+## BBB Server-side settings
+you have to set this on the server for the plugin to receive any data:
+- defaultKeepEvents=true# moodle-bbbext_lad
---
+++ version.php
@@ -0,0 +1,34 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This file contains the version information for the BBB Learning Analytics Dashboard extension.
+ *
+ * @package   bbbext_lad
+ * @copyright 2023 onwards, Blindside Networks Inc
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @author    Laurent David  (laurent [at] call-learning [dt] fr)
+ */
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version      = 2026012601;
+$plugin->requires     = 2023020300;
+$plugin->component    = 'bbbext_lad';
+$plugin->supports     = [404, 502];
+$plugin->dependencies = [
+    'mod_bigbluebuttonbn' => 2023020300,
+    'local_bbb_lad' => 2026012601,
+];
